dad725793aa8c3d660d305e6d7f9d8202efe64ad
Aditya Patil <98159033+Nerditya@users.noreply.github.com>
2023-03-14 21:25:59 +0530

    Added Register File

--- Hardware/register.py
+++ Hardware/register.py
@@ -1,1 +1,16 @@
+class RF():
+    def __init__(self) :
+        self.rf=[0]*32
+        self.rf[2]=2147483612
+        self.rf[3]=268435456
+        self.rf[10]=1
+        self.rf[11]=2147483612
 
+    def write(self,i,val):
+        self.rf[i]=val
+
+    def read(self,i):
+        return self.rf[i]
+
+    def readfile(self):
+        return self.rf
