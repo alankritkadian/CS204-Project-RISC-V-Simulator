c41e8f72b5676927f5c89f14cb06a6eaf0595d95
Aditya Patil <98159033+Nerditya@users.noreply.github.com>
2023-03-14 21:05:16 +0530

    Added decode to processor

--- processor.py
+++ processor.py
@@ -9,8 +9,8 @@ from bitarray.util import ba2int, int2ba,zeros
 
 
 class Environment:
-    def __init__(self):  
-        self.opcode=0      
+    def __init__(self):
+        self.opcode=0
         self.Inst=0
         self.rd=0
         self.rs1=0
@@ -47,7 +47,7 @@ class Processor:
         for i in range(0,len(instructions)):
             print(int(instructions[i],16))
             self.memory.write_word(32*i,int2ba(int(instructions[i],16),length = 32,signed=False))
-    
+
     def return_inst(self):
         print(self.inst_array)
         return self.inst_array
@@ -56,3 +56,48 @@ class Processor:
         self.currInst.Inst=ba2int(self.memory.load_word(self.PC),signed=False) # currinst stores current instruction
         print("Instruction Fetched:- ",self.currInst.Inst)
         return self.currInst.Inst
+
+
+    def decode(self):
+        self.currInst.opcode=self.currInst.Inst&127
+        self.currInst.rd=(self.currInst.Inst>>7)&31
+        self.currInst.funct3=(self.currInst.Inst>>12)&7
+        self.currInst.rs1=(self.currInst.Inst>>15)&31
+        self.currInst.rs2=(self.currInst.Inst>>20)&31
+        self.currInst.funct7=(self.currInst.Inst>>25)&127
+
+        temp = int2ba(self.currInst.Inst,length=32,signed=False)
+
+        imm = temp[0:12]
+        self.currInst.imm=ba2int(imm,signed=True)
+
+        immS = temp[0:7]
+        immS += temp[20:25]
+        self.currInst.immS=ba2int(immS,signed=True)
+
+        immB = bitarray(0)
+        immB.append(temp[0])
+        immB.append(temp[24])
+        immB += temp[1:7]
+        immB += temp[20:24]
+        immB.append(0)
+        # print("Instruction:-",temp)
+        #print("immB:-",immB)
+        self.currInst.immB = ba2int(immB,signed=True)
+
+        immU=temp[0:20]
+        immU+=zeros(12)
+        self.currInst.immU=ba2int(immU,signed=True)
+
+        immJ=bitarray(0)
+        immJ.append(temp[0])
+        immJ+=temp[12:20]
+        immJ.append(temp[11])
+        immJ+=temp[1:11]
+        immJ.append(0)
+        self.currInst.immJ+= ba2int(immJ,signed=True)
+
+        self.currInst.op1=self.RF.read(self.currInst.rs1)
+        self.currInst.op2=self.RF.read(self.currInst.rs2)
+        self.control.Type_gen()
+        #print("Instruction Opcode:- ",self.currInst.opcode)
